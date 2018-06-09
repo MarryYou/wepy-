c477dfec80ded4f4a1ccfba59a30c56baf3d5fba
MarryYou <614345508@qq.com>
2018-06-09 17:41:26 +0800

    wepy初体验

---
+++ src/common/api.js
@@ -0,0 +1,15 @@
+const apiDomain = 'http://106.15.183.98:8360/';
+
+module.exports ={
+    IndexUrl:apiDomain + 'index/index',
+    bannerImage:[
+         'https://yanxuan.nosdn.127.net/a9830a60d3c3d59d9451a4330dcf8f3b.jpg?imageView&thumbnail=750x0&quality=75',
+         'https://yanxuan.nosdn.127.net/14e31d94b2e9bf62d6b353f6a9cea999.jpg?imageView&thumbnail=750x0&quality=75',
+         'https://yanxuan.nosdn.127.net/d94ddc71087d29cd0267848ce8d8fa9b.jpg?imageView&thumbnail=750x0&quality=75',
+         'https://yanxuan.nosdn.127.net/2539197132fde05a6e032b094460f4df.jpg?imageView&thumbnail=750x0&quality=75',
+         'https://yanxuan.nosdn.127.net/407e5c63c01dd405fe04d76ab5967664.jpg?imageView&thumbnail=750x0&quality=75',
+         'https://yanxuan.nosdn.127.net/472bfe7eb974201e2bc3642468796f21.jpg?imageView&thumbnail=750x0&quality=75',
+         'https://yanxuan.nosdn.127.net/16fac1a12dc651da6bb27024eb1d72b5.jpg?imageView&thumbnail=750x0&quality=75',
+         'https://yanxuan.nosdn.127.net/5fc635a97c02fc7b3251f919c400637b.jpg?imageView&thumbnail=750x0&quality=75'
+    ]
+}
